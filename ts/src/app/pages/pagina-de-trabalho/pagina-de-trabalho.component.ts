58ce59f05aff358baee1293f404a1f960de73c77
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-05-10 15:45:12 -0300

    feat: Sessão de discussões - Expandindo respostas

--- ts/src/app/pages/pagina-de-trabalho/pagina-de-trabalho.component.ts
+++ ts/src/app/pages/pagina-de-trabalho/pagina-de-trabalho.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Comentario {
+  autor: string;
+  colaborouTrabalho: boolean;
+  funcao: string;
+  texto: string;
+}
+
 @Component({
   selector: 'app-pagina-de-trabalho',
   templateUrl: './pagina-de-trabalho.component.html',
@@ -27,13 +34,13 @@ export class PaginaDeTrabalhoComponent implements OnInit {
   // 0 = Discussão não iniciada
   // 1 = Escrevendo Discussão
   // 2 = Discussão enviada
-  statusTopico: number = 2;
+  statusTopico: number = 0;
 
   usuarioEmail: string = 'gabrielsoares.pe.br@gmail.com';
   discussaoAssuntoTemp: string = '';
   discussaoConteudoTemp: string = '';
-  discussaoAssunto: string = 'gabriel';
-  discussaoConteudo: string = 'soares';
+  discussaoAssunto: string = '';
+  discussaoConteudo: string = '';
 
   atualizaDiscussao(evento: MouseEvent) {
     evento.preventDefault();
@@ -46,6 +53,43 @@ export class PaginaDeTrabalhoComponent implements OnInit {
       this.discussaoConteudo = this.discussaoConteudoTemp;
     } else if (this.statusTopico == 2) this.statusTopico = 1;
   }
+
+  visibilidadeComentarios: boolean = false;
+
+  toggleComentarios() {
+    this.visibilidadeComentarios = !this.visibilidadeComentarios;
+  }
+
+  comentariosLista: Comentario[] = [
+    {
+      autor: 'Adriano da Silva',
+      colaborouTrabalho: true,
+      funcao: 'Autor',
+      texto:
+        'Resposta do autor é aqui. Relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo.',
+    },
+    {
+      autor: 'Carlos Henrique Santos',
+      colaborouTrabalho: false,
+      funcao: '',
+      texto:
+        'Consegui entender melhor agora! Parece que a variação da análise da dimensão e impacto de processo formativo situado impacto de processo formativo. Obrigada pela resposta, muito interessante o seu trabalho!',
+    },
+    {
+      autor: 'Carmila Ferreira Andrade',
+      colaborouTrabalho: true,
+      funcao: 'Coautor',
+      texto:
+        'Também interessante lembrar que o relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo. Situado impacto de processo formativo processo resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo.',
+    },
+    {
+      autor: 'Ana Carolina',
+      colaborouTrabalho: true,
+      funcao: 'Coautor',
+      texto:
+        'Resposta do autor é aqui. Relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo.',
+    },
+  ];
 
   constructor() {}
 
